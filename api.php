1086f331343db40b6e5a0455e7691ef06bb0c36b
daczecha <87547231+daczecha@users.noreply.github.com>
2021-12-16 19:10:21 +0400

    Add files via upload

--- api.php
+++ api.php
@@ -4,12 +4,12 @@
 
 
     //create array for response
-    $response = [];
+    $response = array();
     
     //check connection
     //
     if($conn){ //if ok generate json
-
+        
         //get data
         $sql = 'SELECT id,title,body,userId FROM posts';
         $result = mysqli_query($conn, $sql);
@@ -26,6 +26,7 @@
             $i++;
         }
 
+        global $json_data;
         $json_data = json_encode($response,JSON_PRETTY_PRINT);
 
     }else{//if not show error 
@@ -33,6 +34,67 @@
     }
 ?>
 
+
+<?php
+    //search in json by key and value
+    function searchJSON($value){
+        $results = array();
+        $decoded = json_decode($GLOBALS['json_data'], true);
+        foreach($decoded as $item){
+            if( 
+                $item->userId == $value ||
+                strpos($item->title, $value) ||
+                strpos($item->body, $value)
+            ){
+                $results[] = $item;
+            }
+        }
+        $results = json_encode($results, JSON_PRETTY_PRINT);
+        return $results;
+    }
+?>
+
+
+
+<?php
+    //sort json data by id or userId
+    function sortJSON($value = 'id', $sortType = 'nr'){ //'nr' stands for 'not reversed',    'r' stands for 'reversed'
+            //sort data
+        $decoded = json_decode($GLOBALS['json_data'], true);
+        
+        if($value == 'id'){
+            if($sortType == 'r'){
+                usort($decoded, function ($a, $b) {
+                    return $b['id'] - $a['id'];
+                });
+            }elseif($sortType == 'nr'){
+                usort($decoded, function ($a, $b) {
+                    return $a['id'] - $b['id'];
+                });
+            }
+            $decoded = json_encode($decoded, JSON_PRETTY_PRINT);
+            return $decoded;
+        }elseif($value == 'userId'){
+            if($sortType == 'r'){
+                usort($decoded, function ($a, $b) {
+                    return $b['userId'] - $a['userId'];
+                });
+            }elseif($sortType == 'nr'){
+                usort($decoded, function ($a, $b) {
+                    return $a['userId'] - $b['userId'];
+                });
+            }
+            $decoded = json_encode($decoded, JSON_PRETTY_PRINT);
+            return $decoded;
+        }else{
+        echo "Cannot sort data by this property.";
+        }
+
+
+    }
+
+?>
+
 <pre>
-    <?php echo $json_data?>
+    <?php echo sortJSON()?>
 </pre>
